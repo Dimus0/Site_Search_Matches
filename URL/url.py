275e923c42a8146e4b2a4d5f4b5327b5f1d5829f
Puhachevskyi Dmytro <106034451+Dimus0@users.noreply.github.com>
2023-11-13 14:39:53 +0200

    Тут працює бд на SQLite.

---
+++ URL/url.py
@@ -0,0 +1,17 @@
+from flask import Flask,redirect,url_for,render_template,request
+
+app = Flask(__name__,template_folder='../template')
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return  redirect(url_for("user", usr=user))
+    else:
+        return render_template("index-login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}<h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from flask import Flask, redirect, url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='../template')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+class Player(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(100),n)
+    numbers_player = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Player: {self.name}, {self.numbers_player}>'
 
 @app.route('/')
 def hello_world():
